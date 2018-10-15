af0a993e29bec7ffe0e8f59548f079935330d491
TechDweebGaming <jakob.conner42@gmail.com>
2018-10-15 16:20:11 -0700

    Basic Setup 1

---
+++ src/main/java/io/github/techdweebgaming/ctunigui/CTUniGUI.java
@@ -0,0 +1,10 @@
+package io.github.techdweebgaming.ctunigui;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(useMetadata = true, modid = CTUniGUI.modID)
+public class CTUniGUI {
+	
+	public static final String modID = "ctunigui";
+
+}
